0f36fb8f99d83d89204ae87255efeac603e7e433
Yomar Miranda <mirandayomar@gmail.com>
2018-12-11 18:13:34 -0500

    24 - Multiply and divide on child component.

--- src/app/counter/child/child.component.ts
+++ src/app/counter/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChildComponent implements OnInit {
 
+  @Input() counter: number;
+  @Output() changeCounter = new EventEmitter<number>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  multiply() {
+    this.counter *= 2;
+    this.changeCounter.emit(this.counter);
+  }
+
+  divide() {
+    this.counter /= 2;
+    this.changeCounter.emit(this.counter);
   }
 
 }
