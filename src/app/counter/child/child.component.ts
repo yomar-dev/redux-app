848c67bb1b0e2a3b8b0ca94c495a66f7dc0aaeda
Yomar Miranda <mirandayomar@gmail.com>
2018-12-11 17:17:38 -0500

    24 - Create child component.

---
+++ src/app/counter/child/child.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styles: []
+})
+export class ChildComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
