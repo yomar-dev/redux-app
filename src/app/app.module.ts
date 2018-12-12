90872c956fdf4c051c4d5f0490b41b02402ec632
Yomar Miranda <mirandayomar@gmail.com>
2018-12-12 14:27:39 -0500

    28 - Install and config Store DevTools.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { counterReducer } from './counter/counter.reducer';
 
 import { AppComponent } from './app.component';
 import { ChildComponent } from './counter/child/child.component';
 import { GrandchildComponent } from './counter/grandchild/grandchild.component';
-import { counterReducer } from './counter/counter.reducer';
+
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,11 @@ import { counterReducer } from './counter/counter.reducer';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({ counter: counterReducer })
+    StoreModule.forRoot({ counter: counterReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production // Restrict extension to log-only mode
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
